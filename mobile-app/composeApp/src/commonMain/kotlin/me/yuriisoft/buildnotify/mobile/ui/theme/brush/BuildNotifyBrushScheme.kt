6ba1dfdaeaf84d24a368c8e1c1e157155cca66bd
Yurii Surzhykov <yuriisurzhykov@gmail.com>
2026-03-23 21:17:15 -0700

    refactor: Rename GradientSpec subclasses and add Size-aware Brush generation
    Signed-off-by: Yurii Surzhykov <yuriisurzhykov@gmail.com>

--- mobile-app/composeApp/src/commonMain/kotlin/me/yuriisoft/buildnotify/mobile/ui/theme/brush/BuildNotifyBrushScheme.kt
+++ mobile-app/composeApp/src/commonMain/kotlin/me/yuriisoft/buildnotify/mobile/ui/theme/brush/BuildNotifyBrushScheme.kt
@@ -4,22 +4,22 @@ import me.yuriisoft.buildnotify.mobile.ui.theme.Palette
 import me.yuriisoft.buildnotify.mobile.ui.theme.gradient.GradientSpec
 
 val LightBrushScheme = BrushScheme(
-    actionGradient = GradientSpec.LinearGradientSpec(
+    actionGradient = GradientSpec.Linear(
         colors = listOf(Palette.Blue500, Palette.Violet500),
         angle = 45f,
     ),
-    progressGradient = GradientSpec.LinearGradientSpec(
+    progressGradient = GradientSpec.Linear(
         colors = listOf(Palette.Green500, Palette.Lime500),
         angle = 0f,
     ),
 )
 
 val DarkBrushScheme = BrushScheme(
-    actionGradient = GradientSpec.LinearGradientSpec(
+    actionGradient = GradientSpec.Linear(
         colors = listOf(Palette.Blue500, Palette.Violet500),
         angle = 45f,
     ),
-    progressGradient = GradientSpec.LinearGradientSpec(
+    progressGradient = GradientSpec.Linear(
         colors = listOf(Palette.Green500, Palette.Lime500),
         angle = 0f,
     ),
